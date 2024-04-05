5ca26638cb80ea835ddc63045b89b0803d5eb6f8
Ceana Potter <144651127+Ceana-Potter13@users.noreply.github.com>
2024-04-05 14:04:12 +0000

    Current Changes

--- server.js
+++ server.js
@@ -6,3 +6,31 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/financeData", function (req, res) {
+    const simulatedData = [
+        {
+          title: "March Grocery",
+          category: "Groceries",
+          amount: "$200",
+          type: "Debit",
+          date: "2024-04-01",
+        },
+        {
+          title: "Utility Bill",
+          category: "Utilities",
+          amount: "$150",
+          type: "Debit",
+          date: "2024-04-02",
+        },
+        {
+          title: "Movie Night",
+          category: "Entertainment",
+          amount: "$100",
+          type: "Debit",
+          date: "2024-04-03",
+        },
+      ];
+    res.type("json");
+    res.send(simulatedData);
+  });
